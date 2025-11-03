92f87b60242b1507f4a9a8e6313f91a3b57ce68f
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-03 14:32:38 +0100

    TRC-93 TRC-94 Ajout de la classe principale TricolSupplyProBootApplication pour démarrer l'application Spring Boot

---
+++ src/main/java/com/tricol/supply/TricolSupplyProBootApplication.java
@@ -0,0 +1,13 @@
+package com.tricol.supply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricolSupplyProBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricolSupplyProBootApplication.class, args);
+	}
+
+}
