f83cfa600b9593af07e2aab0182e89f252fbf18b
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-05 11:59:31 +0100

    TRC-3 Ajout des attributs et annotations de validation dans la classe FournisseurDTO

--- src/main/java/com/tricol/supply/dto/FournisseurDTO.java
+++ src/main/java/com/tricol/supply/dto/FournisseurDTO.java
@@ -7,6 +7,22 @@ import lombok.Data;
 @Data
 public class FournisseurDTO {
     
+    private Long id;
     
+    @NotBlank(message = "Le nom de la société est obligatoire")
+    private String societe;
+    
+    private String adresse;
+    
+    private String contact;
+    
+    @Email(message = "Email invalide")
+    private String email;
+    
+    private String telephone;
+    
+    private String ville;
+    
+    private String ice;
 }
 
