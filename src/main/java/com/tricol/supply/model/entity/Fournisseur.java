4ad698ce11c289ecc242417db588e398f5b4c553
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-05 11:50:55 +0100

    TRC-2 Ajout des attributs et annotations JPA dans la classe Fournisseur

--- src/main/java/com/tricol/supply/model/entity/Fournisseur.java
+++ src/main/java/com/tricol/supply/model/entity/Fournisseur.java
@@ -16,6 +16,37 @@ import java.util.List;
 @Builder
 public class Fournisseur {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String societe;
+
+    @Column(nullable = false, length = 500)
+    private String adresse;
+
+    @Column(nullable = false, length = 255)
+    private String contact;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String telephone;
+
+    @Column(nullable = false, length = 100)
+    private String ville;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String ice;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     
 }
 
