1ab6ddf5a1a73dad56d2818e3e2c5bd154df94ff
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-05 12:00:28 +0100

    TRC-3 Ajout de l'interface FournisseurMapper pour la conversion entre Fournisseur et FournisseurDTO

---
+++ src/main/java/com/tricol/supply/mapper/FournisseurMapper.java
@@ -0,0 +1,15 @@
+package com.tricol.supply.mapper;
+
+import com.tricol.supply.dto.FournisseurDTO;
+import com.tricol.supply.model.entity.Fournisseur;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface FournisseurMapper {
+    
+    
+}
+
