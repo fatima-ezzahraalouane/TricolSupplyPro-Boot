e64a7b5850a8da9bbfd646c989fd41a8de993e99
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-05 11:54:05 +0100

    TRC-2 Ajout de l'interface FournisseurRepository pour la gestion des entités Fournisseur

---
+++ src/main/java/com/tricol/supply/repository/FournisseurRepository.java
@@ -0,0 +1,10 @@
+package com.tricol.supply.repository;
+
+import com.tricol.supply.model.entity.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+}
+
