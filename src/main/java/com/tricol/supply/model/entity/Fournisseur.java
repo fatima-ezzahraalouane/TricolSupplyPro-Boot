a313a032d50b6daf40177b734c4c029c3f466657
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-05 11:49:51 +0100

    TRC-2 Ajout de la classe Fournisseur avec annotations JPA et Lombok

---
+++ src/main/java/com/tricol/supply/model/entity/Fournisseur.java
@@ -0,0 +1,21 @@
+package com.tricol.supply.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "fournisseurs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Fournisseur {
+
+    
+}
+
