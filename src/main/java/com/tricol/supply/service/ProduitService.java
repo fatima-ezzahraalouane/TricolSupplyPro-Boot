9357789f9cca28e5a06d7c5dbd218fe2049f48b0
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:23:38 +0100

    TRC-25 Ajout de la classe ProduitService pour gérer la logique métier liée aux Produits

---
+++ src/main/java/com/tricol/supply/service/ProduitService.java
@@ -0,0 +1,20 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.dto.ProduitDTO;
+import com.tricol.supply.model.entity.Produit;
+import com.tricol.supply.exception.ResourceNotFoundException;
+import com.tricol.supply.mapper.ProduitMapper;
+import com.tricol.supply.repository.ProduitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProduitService {
+    
+    
+}
+
