6ec194ed896859cd03da3251228d3ced336f4062
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:44:59 +0100

    TRC-40 Ajout de la classe CommandeFournisseur avec annotations JPA et Lombok

---
+++ src/main/java/com/tricol/supply/model/entity/CommandeFournisseur.java
@@ -0,0 +1,23 @@
+package com.tricol.supply.model.entity;
+
+import com.tricol.supply.model.enums.StatutCommande;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "commandes_fournisseur")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommandeFournisseur {
+
+    
+}
+
