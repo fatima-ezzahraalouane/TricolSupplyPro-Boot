80a6591d3e71f6ebedad75d9a897b5b4c17b2ac6
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:12:42 +0100

    TRC-24 TRC-40 Ajout de la classe ProduitCommandeDTO pour représenter les détails d'un produit dans une commande

---
+++ src/main/java/com/tricol/supply/dto/ProduitCommandeDTO.java
@@ -0,0 +1,12 @@
+package com.tricol.supply.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ProduitCommandeDTO {
+    
+    
+}
+
