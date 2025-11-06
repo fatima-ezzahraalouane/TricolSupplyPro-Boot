367d96fcb4e5f7f1390f08c719ad7de180280fc7
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:18:04 +0100

    TRC-24 Ajout de l'interface ProduitMapper pour la conversion entre Produit et ProduitDTO

---
+++ src/main/java/com/tricol/supply/mapper/ProduitMapper.java
@@ -0,0 +1,15 @@
+package com.tricol.supply.mapper;
+
+import com.tricol.supply.dto.ProduitDTO;
+import com.tricol.supply.model.entity.Produit;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface ProduitMapper {
+    
+    
+}
+
