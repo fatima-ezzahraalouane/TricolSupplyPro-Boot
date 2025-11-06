43c978d84332a8958f2154dc7cdb6f26f7278306
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:04:57 +0100

    TRC-24 Ajout de l'interface CommandeProduitRepository pour gérer les opérations sur CommandeProduit

---
+++ src/main/java/com/tricol/supply/repository/CommandeProduitRepository.java
@@ -0,0 +1,11 @@
+package com.tricol.supply.repository;
+
+import com.tricol.supply.model.entity.CommandeProduit;
+import com.tricol.supply.model.entity.CommandeProduitId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommandeProduitRepository extends JpaRepository<CommandeProduit, CommandeProduitId> {
+}
+
