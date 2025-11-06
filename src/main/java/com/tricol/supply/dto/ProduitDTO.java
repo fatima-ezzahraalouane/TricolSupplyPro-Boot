e6823dff9eb81a598a7c435ea741415d20193ed6
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:16:58 +0100

    TRC-24 Ajout de la classe ProduitDTO avec annotations de validation et Lombok

---
+++ src/main/java/com/tricol/supply/dto/ProduitDTO.java
@@ -0,0 +1,14 @@
+package com.tricol.supply.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProduitDTO {
+    
+    
+}
+
