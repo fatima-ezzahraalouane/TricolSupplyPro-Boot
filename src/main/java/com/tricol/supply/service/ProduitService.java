91817564ac398edac9037f9f416226425e625f17
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:25:24 +0100

    TRC-25 Ajout de la méthode findAll dans la classe ProduitService pour récupérer tous les Produits sous forme de page de ProduitDTO

--- src/main/java/com/tricol/supply/service/ProduitService.java
+++ src/main/java/com/tricol/supply/service/ProduitService.java
@@ -18,6 +18,11 @@ public class ProduitService {
     private final ProduitRepository produitRepository;
     private final ProduitMapper produitMapper;
     
+    public Page<ProduitDTO> findAll(Pageable pageable) {
+        return produitRepository.findAll(pageable)
+            .map(produitMapper::toDTO);
+    }
+    
     
 }
 
