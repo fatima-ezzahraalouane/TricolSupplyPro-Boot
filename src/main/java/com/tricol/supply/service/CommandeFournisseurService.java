4fff69373b02b22294879d0b194afce1c5cfa390
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:24:58 +0100

    TRC-41 Ajout de la dépendance MouvementStockRepository dans la classe CommandeFournisseurService

--- src/main/java/com/tricol/supply/service/CommandeFournisseurService.java
+++ src/main/java/com/tricol/supply/service/CommandeFournisseurService.java
@@ -26,6 +26,7 @@ public class CommandeFournisseurService {
     private final FournisseurRepository fournisseurRepository;
     private final ProduitRepository produitRepository;
     private final CommandeProduitRepository commandeProduitRepository;
+    private final MouvementStockRepository mouvementStockRepository;
     
 }
 
