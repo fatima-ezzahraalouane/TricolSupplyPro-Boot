341264ace27ce0fdbedafb5aed70eff4ae180ab0
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:15:50 +0100

    TRC-24 Ajout de l'interface ProduitRepository pour la gestion des entités Produit

---
+++ src/main/java/com/tricol/supply/repository/ProduitRepository.java
@@ -0,0 +1,10 @@
+package com.tricol.supply.repository;
+
+import com.tricol.supply.model.entity.Produit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long> {
+}
+
