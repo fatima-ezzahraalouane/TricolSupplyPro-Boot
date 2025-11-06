9fb4e5e2779ce5b4098571e69fe4762d0f21917f
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:10:22 +0100

    TRC-24 TRC-40 Ajout de la classe CommandeFournisseurDetailDTO pour représenter les détails d'une commande fournisseur

---
+++ src/main/java/com/tricol/supply/dto/CommandeFournisseurDetailDTO.java
@@ -0,0 +1,16 @@
+package com.tricol.supply.dto;
+
+import com.tricol.supply.model.enums.StatutCommande;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CommandeFournisseurDetailDTO {
+    
+    
+}
+
