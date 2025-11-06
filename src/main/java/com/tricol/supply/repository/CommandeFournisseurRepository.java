c663c6898873d8a265fe28401c3267f372fad967
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:58:24 +0100

    TRC-41 Ajout de l'interface CommandeFournisseurRepository pour gérer les opérations sur les commandes fournisseurs

---
+++ src/main/java/com/tricol/supply/repository/CommandeFournisseurRepository.java
@@ -0,0 +1,14 @@
+package com.tricol.supply.repository;
+
+import com.tricol.supply.model.entity.CommandeFournisseur;
+import com.tricol.supply.model.entity.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommandeFournisseurRepository extends JpaRepository<CommandeFournisseur, Long> {
+    
+}
+
