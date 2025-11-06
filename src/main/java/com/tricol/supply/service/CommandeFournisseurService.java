85639658b5ce5bf10c15ac269b7ee48bae527351
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:25:23 +0100

    TRC-41 Ajout de la dépendance CommandeFournisseurMapper dans la classe CommandeFournisseurService

--- src/main/java/com/tricol/supply/service/CommandeFournisseurService.java
+++ src/main/java/com/tricol/supply/service/CommandeFournisseurService.java
@@ -27,6 +27,7 @@ public class CommandeFournisseurService {
     private final ProduitRepository produitRepository;
     private final CommandeProduitRepository commandeProduitRepository;
     private final MouvementStockRepository mouvementStockRepository;
+    private final CommandeFournisseurMapper commandeMapper;
     
 }
 
