22a49e2fa3f24dcc71b7866962a95eda6ba893f3
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 18:13:21 +0100

    TRC-24 Ajout de la classe Produit avec annotations JPA et Lombok

---
+++ src/main/java/com/tricol/supply/model/entity/Produit.java
@@ -0,0 +1,22 @@
+package com.tricol.supply.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "produits")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Produit {
+
+    
+}
+
