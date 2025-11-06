0d8aea157ed2b6df22bcdc27f2e0a122ccb08ff6
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:19:17 +0100

    TRC-41 Ajout de la classe CommandeFournisseurService pour gérer les opérations liées aux commandes fournisseurs

---
+++ src/main/java/com/tricol/supply/service/CommandeFournisseurService.java
@@ -0,0 +1,25 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.dto.*;
+import com.tricol.supply.model.entity.*;
+import com.tricol.supply.model.enums.StatutCommande;
+import com.tricol.supply.model.enums.TypeMouvement;
+import com.tricol.supply.exception.ResourceNotFoundException;
+import com.tricol.supply.mapper.CommandeFournisseurMapper;
+import com.tricol.supply.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommandeFournisseurService {
+    
+    
