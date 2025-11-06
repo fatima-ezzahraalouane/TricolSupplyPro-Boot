1bb4ef924f666b472453949c2aa88e7e5526ced7
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:03:37 +0100

    TRC-41 Ajout des attributs et des annotations nécessaires dans la classe CommandeFournisseurDTO

--- src/main/java/com/tricol/supply/dto/CommandeFournisseurDTO.java
+++ src/main/java/com/tricol/supply/dto/CommandeFournisseurDTO.java
@@ -13,6 +13,19 @@ import java.util.List;
 @Data
 public class CommandeFournisseurDTO {
     
+    private Long id;
     
+    private LocalDateTime dateCommande;
+    
+    @NotNull(message = "Le statut est obligatoire")
+    private StatutCommande statut;
+    
+    private BigDecimal montantTotal;
+    
+    @NotNull(message = "Le fournisseur est obligatoire")
+    private Long fournisseurId;
+    
+    @NotEmpty(message = "Au moins un produit doit être associé")
+    private List<ProduitCommandeDTO> produits = new ArrayList<>();
 }
 
