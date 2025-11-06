f3676ba018cb940cee15588e3d9eabbbf9baaf48
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:13:08 +0100

    TRC-24 TRC-40 Ajout des attributs produitId, quantite et prixUnitaireCommande dans la classe ProduitCommandeDTO

--- src/main/java/com/tricol/supply/dto/ProduitCommandeDTO.java
+++ src/main/java/com/tricol/supply/dto/ProduitCommandeDTO.java
@@ -7,6 +7,14 @@ import lombok.Data;
 @Data
 public class ProduitCommandeDTO {
     
+    @NotNull(message = "L'ID du produit est obligatoire")
+    private Long produitId;
     
+    @Min(value = 1, message = "La quantité doit être supérieure à 0")
+    @NotNull(message = "La quantité est obligatoire")
+    private Integer quantite;
+    
+    @NotNull(message = "Le prix unitaire de la commande est obligatoire")
+    private java.math.BigDecimal prixUnitaireCommande;
 }
 
