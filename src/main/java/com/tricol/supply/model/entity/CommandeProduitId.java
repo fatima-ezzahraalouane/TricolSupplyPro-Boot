eed7e4e38f243e1bf6cb6ffd82bc303d8ede7eb7
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:08:22 +0100

    TRC-24 TRC-40 Ajout de la classe CommandeProduitId pour gérer l'identifiant composite de CommandeProduit

---
+++ src/main/java/com/tricol/supply/model/entity/CommandeProduitId.java
@@ -0,0 +1,16 @@
+package com.tricol.supply.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommandeProduitId implements Serializable {
+
+    
+}
+
