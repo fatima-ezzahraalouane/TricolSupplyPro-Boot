34bc1397bc57bf27874d8c7c1299713a84b35851
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-06 19:03:17 +0100

    TRC-41 Ajout de la classe CommandeFournisseurDTO avec annotations et imports nécessaires

---
+++ src/main/java/com/tricol/supply/dto/CommandeFournisseurDTO.java
@@ -0,0 +1,18 @@
+package com.tricol.supply.dto;
+
+import com.tricol.supply.model.enums.StatutCommande;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class CommandeFournisseurDTO {
+    
+    
+}
+
