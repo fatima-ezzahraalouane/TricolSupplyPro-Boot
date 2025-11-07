4f1cb13359ba1fe87d5be7348b883e9fe94743b5
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:02:53 +0100

    TRC-59 Ajout de la classe MouvementStock avec annotations JPA et Lombok

---
+++ src/main/java/com/tricol/supply/model/entity/MouvementStock.java
@@ -0,0 +1,21 @@
+package com.tricol.supply.model.entity;
+
+import com.tricol.supply.model.enums.TypeMouvement;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "mouvements_stock")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MouvementStock {
+
+    
+}
+
