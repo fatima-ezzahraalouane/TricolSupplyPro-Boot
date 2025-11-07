c41725b5e886a5468eceeda93bfcc30da1ffc240
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:15:43 +0100

    TRC-60 Ajout de la classe MouvementStockService pour gérer la logique métier liée aux mouvements de stock

---
+++ src/main/java/com/tricol/supply/service/MouvementStockService.java
@@ -0,0 +1,23 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.dto.MouvementStockDTO;
+import com.tricol.supply.model.entity.CommandeFournisseur;
+import com.tricol.supply.model.entity.MouvementStock;
+import com.tricol.supply.model.entity.Produit;
+import com.tricol.supply.exception.ResourceNotFoundException;
+import com.tricol.supply.mapper.MouvementStockMapper;
+import com.tricol.supply.repository.CommandeFournisseurRepository;
+import com.tricol.supply.repository.MouvementStockRepository;
+import com.tricol.supply.repository.ProduitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MouvementStockService {
+    
+    
+}
+
