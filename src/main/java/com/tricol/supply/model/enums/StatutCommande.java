5ca678bf85cab3af9f1150b78d4bfd79ed0a6bf5
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 19:31:19 +0100

    TRC-40 Mise à jour de l'énumération StatutCommande

--- src/main/java/com/tricol/supply/model/enums/StatutCommande.java
+++ src/main/java/com/tricol/supply/model/enums/StatutCommande.java
@@ -1,28 +1,9 @@
 package com.tricol.supply.model.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
-
 public enum StatutCommande {
-    EN_ATTENTE("En attente"),
-    VALIDEE("Validée"),
-    LIVREE("Livrée"),
-    ANNULEE("Annulée");
-
-    private final String libelle;
-
-    StatutCommande(String libelle) {
-        this.libelle = libelle;
-    }
-
-    @JsonValue
-    public String getLibelle() {
-        return libelle;
-    }
-
-    @Override
-    public String toString() {
-        return libelle;
-    }
+    EN_ATTENTE,
+    VALIDEE,
+    LIVREE,
+    ANNULEE
 }
 
