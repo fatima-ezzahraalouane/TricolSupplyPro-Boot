8fc93e3f417a1754bdcdbacb226cf85624127f16
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:05:44 +0100

    TRC-59 Ajout des relations ManyToOne pour les entités Produit et CommandeFournisseur dans la classe MouvementStock

--- src/main/java/com/tricol/supply/model/entity/MouvementStock.java
+++ src/main/java/com/tricol/supply/model/entity/MouvementStock.java
@@ -33,6 +33,14 @@ public class MouvementStock {
     @Column(name = "prix_unitaire", nullable = false, precision = 19, scale = 2)
     private BigDecimal prixUnitaire;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produit_id", nullable = false)
+    private Produit produit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "commande_fournisseur_id")
+    private CommandeFournisseur commandeFournisseur;
+
     
 }
 
