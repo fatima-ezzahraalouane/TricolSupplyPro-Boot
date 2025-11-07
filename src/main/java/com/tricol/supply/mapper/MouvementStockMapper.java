9e30297e8b544a9fc22002d8b52ac2853ac14240
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:17:55 +0100

    TRC-60 Ajout de l'interface MouvementStockMapper pour la conversion entre les entités et les DTO de mouvements de stock

---
+++ src/main/java/com/tricol/supply/mapper/MouvementStockMapper.java
@@ -0,0 +1,15 @@
+package com.tricol.supply.mapper;
+
+import com.tricol.supply.dto.MouvementStockDTO;
+import com.tricol.supply.model.entity.MouvementStock;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MouvementStockMapper {
+    
+    
+}
+
