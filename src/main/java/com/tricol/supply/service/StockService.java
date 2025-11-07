4562a26cb5cff468ae7b4e022ef4d8e1ad98e306
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:21:22 +0100

    TRC-60 Ajout de la classe StockService pour gérer les opérations liées aux produits en stock

---
+++ src/main/java/com/tricol/supply/service/StockService.java
@@ -0,0 +1,19 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.model.entity.Produit;
+import com.tricol.supply.repository.ProduitRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+    
+    
+    
+}
+
