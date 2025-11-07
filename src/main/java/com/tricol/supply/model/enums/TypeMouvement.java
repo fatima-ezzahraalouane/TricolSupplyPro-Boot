fb638f879d816be88735ebbf02068808ff38454f
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 19:31:50 +0100

    TRC-59 Mise à jour l'énumération TypeMouvement

--- src/main/java/com/tricol/supply/model/enums/TypeMouvement.java
+++ src/main/java/com/tricol/supply/model/enums/TypeMouvement.java
@@ -1,27 +1,8 @@
 package com.tricol.supply.model.enums;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
-
 public enum TypeMouvement {
-    ENTREE("Entrée"),
-    SORTIE("Sortie"),
-    AJUSTEMENT("Ajustement");
-
-    private final String libelle;
-
-    TypeMouvement(String libelle) {
-        this.libelle = libelle;
-    }
-
-    @JsonValue
-    public String getLibelle() {
-        return libelle;
-    }
-
-    @Override
-    public String toString() {
-        return libelle;
-    }
+    ENTREE,
+    SORTIE,
+    AJUSTEMENT
 }
 
