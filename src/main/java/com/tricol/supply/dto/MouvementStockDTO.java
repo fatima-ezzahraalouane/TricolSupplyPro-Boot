0fd7f3895f1ae815f460e7ea3e0ee21a544c528d
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 12:13:00 +0100

    TRC-60 Ajout de la classe MouvementStockDTO avec annotations Lombok

---
+++ src/main/java/com/tricol/supply/dto/MouvementStockDTO.java
@@ -0,0 +1,14 @@
+package com.tricol.supply.dto;
+
+import com.tricol.supply.model.enums.TypeMouvement;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class MouvementStockDTO {
+    
+    
+}
+
