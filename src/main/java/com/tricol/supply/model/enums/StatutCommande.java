d29f15b2f8bd4adadbf3e95ae5ead0c774b58c85
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 11:52:58 +0100

    TRC-40 Mise à jour l'énumération StatutCommande

--- src/main/java/com/tricol/supply/model/enums/StatutCommande.java
+++ src/main/java/com/tricol/supply/model/enums/StatutCommande.java
@@ -2,9 +2,7 @@ package com.tricol.supply.model.enums;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
-/**
- * Énumération des statuts d'une commande fournisseur
- */
+
 public enum StatutCommande {
     EN_ATTENTE("En attente"),
     VALIDEE("Validée"),
