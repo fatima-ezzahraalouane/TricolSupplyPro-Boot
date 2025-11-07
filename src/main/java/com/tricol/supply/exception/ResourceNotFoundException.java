4fb41ba101e1529a4656dd1726294581011c89b4
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-07 14:08:24 +0100

    TRC-86 Ajoute un second constructeur à ResourceNotFoundException pour formater le message d'erreur avec le nom de la ressource et son ID

--- src/main/java/com/tricol/supply/exception/ResourceNotFoundException.java
+++ src/main/java/com/tricol/supply/exception/ResourceNotFoundException.java
@@ -6,6 +6,8 @@ public class ResourceNotFoundException extends RuntimeException {
         super(message);
     }
     
-    
+    public ResourceNotFoundException(String resource, Long id) {
+        super(String.format("%s avec l'ID %d n'a pas été trouvé", resource, id));
+    }
 }
 
