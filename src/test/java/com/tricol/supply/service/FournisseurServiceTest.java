67fa94bb807f9eed28d8283d0541dfaa40304f6c
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-14 21:38:33 +0100

    TRC-105 Ajout de la classe de test unitaire pour FournisseurService

---
+++ src/test/java/com/tricol/supply/service/FournisseurServiceTest.java
@@ -0,0 +1,35 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.dto.FournisseurDTO;
+import com.tricol.supply.exception.ResourceNotFoundException;
+import com.tricol.supply.mapper.FournisseurMapper;
+import com.tricol.supply.model.entity.Fournisseur;
+import com.tricol.supply.repository.FournisseurRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Tests unitaires pour FournisseurService")
+class FournisseurServiceTest {
+
+    
+}
+
