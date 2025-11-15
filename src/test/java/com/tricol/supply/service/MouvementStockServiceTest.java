cc7e9f64f224f8a89bea3a15d5f235aa2c84a095
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-15 20:46:19 +0100

    TRC-122 Ajout de la classe de test unitaire pour MouvementStockService

---
+++ src/test/java/com/tricol/supply/service/MouvementStockServiceTest.java
@@ -0,0 +1,40 @@
+package com.tricol.supply.service;
+
+import com.tricol.supply.dto.MouvementStockDTO;
+import com.tricol.supply.exception.ResourceNotFoundException;
+import com.tricol.supply.mapper.MouvementStockMapper;
+import com.tricol.supply.model.entity.CommandeFournisseur;
+import com.tricol.supply.model.entity.Fournisseur;
+import com.tricol.supply.model.entity.MouvementStock;
+import com.tricol.supply.model.entity.Produit;
+import com.tricol.supply.model.enums.StatutCommande;
+import com.tricol.supply.model.enums.TypeMouvement;
+import com.tricol.supply.repository.CommandeFournisseurRepository;
+import com.tricol.supply.repository.MouvementStockRepository;
+import com.tricol.supply.repository.ProduitRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Tests unitaires pour MouvementStockService")
+class MouvementStockServiceTest {
+
+    
+}
+
+
