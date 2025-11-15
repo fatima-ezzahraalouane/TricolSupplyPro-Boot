cd366856b7d28e258a205d3acd18ce73644a0365
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-15 21:36:54 +0100

    TRC-128 Ajout d'un test d'intégration pour MouvementStockController

---
+++ src/test/java/com/tricol/supply/integration/MouvementStockControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.tricol.supply.integration;
+
+import com.tricol.supply.model.entity.*;
+import com.tricol.supply.model.enums.StatutCommande;
+import com.tricol.supply.model.enums.TypeMouvement;
+import com.tricol.supply.repository.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+@DisplayName("Tests d'intégration pour MouvementStockController")
+class MouvementStockControllerIntegrationTest {
+
+    
+}
+
+
