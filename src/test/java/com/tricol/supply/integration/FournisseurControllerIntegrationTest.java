7e63735330198166db5b1942f89ec2ce3fedf412
fatima-ezzahraalouane <falouane38@gmail.com>
2025-11-15 21:04:22 +0100

    TRC-128 Ajout d'un test d'intégration pour FournisseurController

---
+++ src/test/java/com/tricol/supply/integration/FournisseurControllerIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.tricol.supply.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tricol.supply.dto.FournisseurDTO;
+import com.tricol.supply.model.entity.Fournisseur;
+import com.tricol.supply.repository.FournisseurRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+@DisplayName("Tests d'intégration pour FournisseurController")
+class FournisseurControllerIntegrationTest {
+
+    
+}
+
